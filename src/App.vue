ba7174431a3d61f0dcddf0373b6c9eb7f05fe9c5
Peter Tarsoly <ptarsoly@Peters-MBP-39.wifi.gsu.edu>
2019-03-31 10:30:10 -0400

    final peter push

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,16 @@
     <button v-if="state.num == 1 || state.num == 2" v-on:click="addInfo()">Next</button>
     <p v-if="state.num == 3">How do you need help?</p>
     <p v-if="state.num == 4">How would you like to provide help?</p>
-    <textarea v-model="message" v-if="state.num == 3 || state.num == 4" />
+    <textarea v-model="message" v-if="state.num == 3 || state.num == 4"/>
     <button v-if="state.num == 3 || state.num == 4" v-on:click="addMessage()">Next</button>
-    
-
+    <h1 v-if="state.num == 5">Thank you for stopping by! Someone will reach out shortly.</h1>
+    <button v-if="state.num == 5" v-on:click="reset()">Finish</button>
   </div>
 </template>
 
 <script lang='ts'>
 import { Vue, Component } from "vue-property-decorator";
+import axios from "axios";
 
 @Component
 export default class App extends Vue {
@@ -30,9 +31,20 @@ export default class App extends Vue {
     num: 0,
     value: ""
   };
+  postToDatabase = {
+    postid: "",
+    name: "",
+    phonenumber: "",
+    email: "",
+    looking: true,
+    offering: false,
+    message: "",
+    imageurl: "https://media.giphy.com/media/bkfedFXLulc8c2ucCI/giphy.gif"
+  };
 
   needHelp() {
     this.state.num = 1;
+    this.state.value = "need";
   }
 
   giveHelp() {
@@ -41,22 +53,53 @@ export default class App extends Vue {
 
   addInfo() {
     console.log(this.personName);
-    if(this.state.num == 1){
+    if (this.state.num == 1) {
       this.state.num = 3; //need help
-    }
-    else if(this.state.num == 2) {
+    } else if (this.state.num == 2) {
       this.state.num = 4; //give help
     }
   }
 
   addMessage() {
     console.log(this.message);
-    if(this.state.num == 3){
+    if (this.state.num == 3) {
       this.state.num = 5; //need help
-    }
-    else if(this.state.num == 4) {
+    } else if (this.state.num == 4) {
       this.state.num = 5; //give help
     }
+  }
+
+  reset() {
+    this.postToDatabase.postid = "42";
+    this.postToDatabase.name = this.personName;
+    this.postToDatabase.phonenumber = this.number;
+    this.postToDatabase.email = this.email;
+    if (this.state.value == "need") {
+      this.postToDatabase.looking = true;
+      this.postToDatabase.offering = false;
+    } else {
+      this.postToDatabase.looking = false;
+      this.postToDatabase.offering = true;
+    }
+    this.postToDatabase.message = this.message;
+
+const b = {
+      method: "post",
+      url: "http://ab6fa199.ngrok.io/insert",
+      data: this.postToDatabase
+};
+    axios(b) 
+      .then(function(response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+
+    this.state.num = 0;
+    this.state.value = "";
   }
 }
 </script>
